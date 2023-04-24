f7a2e98eeac7742be19d0a2f5a49527242c34b4b
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2023-04-24 18:02:22 +0330

    1- Creating Model Currency 2- Using Data fake by model

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:world_currency/model/Currency.dart';
 
 void main() {
   runApp(const MyApp());
@@ -142,7 +143,7 @@ class Home extends StatelessWidget {
                   physics: const BouncingScrollPhysics(),
                   itemCount: 20,
                   itemBuilder: (BuildContext context, int position) {
-                    return const MyItem();
+                    return  MyItem();
                   },
                   separatorBuilder: (BuildContext context, int index) {
                     if (index % 9 == 0) {
@@ -218,12 +219,20 @@ void _showSnackBar(BuildContext context, String msg) {
 }
 
 class MyItem extends StatelessWidget {
-  const MyItem({
+   MyItem({
     super.key,
   });
+  List<Currency> currency = [];
 
   @override
   Widget build(BuildContext context) {
+    currency.add(Currency(id: '1', title: 'دلار آمریکا', price: '25000', change: '+0.2', status: 'p'));
+    currency.add(Currency(id: '2', title: 'دلار استرالیا', price: '76000', change: '+0.2', status: 'p'));
+    currency.add(Currency(id: '3', title: 'دلار یوگن', price: '13000', change: '+0.5', status: 'p'));
+    currency.add(Currency(id: '4', title: 'دلار سترلی', price: '95000', change: '-0.2', status: 'n'));
+    currency.add(Currency(id: '5', title: 'دلار عتیسی', price: '215000', change: '+0.2', status: 'p'));
+    currency.add(Currency(id: '6', title: 'دلار زکی', price: '52000', change: '-0.8', status: 'n'));
+
     return Padding(
       padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
       child: Container(
@@ -243,15 +252,15 @@ class MyItem extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(
-              'دلار',
+              currency[1].title!,
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             Text(
-              '160000',
+              currency[1].price!,
               style: Theme.of(context).textTheme.headlineSmall,
             ),
             Text(
-              '+3',
+              currency[1].change!,
               style: Theme.of(context).textTheme.headlineSmall,
             ),
           ],
---
+++ lib/model/Currency.dart
@@ -0,0 +1,15 @@
+class Currency{
+  String? id;
+  String? title;
+  String? price;
+  String? change;
+  String? status;
+
+  Currency({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.change,
+    required this.status,
+  });
+}
